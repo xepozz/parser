c1c4577a27a50606af4d3e2d3b87d672432efceb
Derepko Dmitriy <xepozz@list.ru>
2017-11-01 20:00:50 +0300

    fix

--- src/WebPage/Mapper.php
+++ src/WebPage/Mapper.php
@@ -20,8 +20,10 @@ class Mapper
         foreach ($map as $name => $value) {
             if (isset($options[$name]) && !is_array($options[$name])) {
                 $merged[$name] = $options[$name];
-            }elseif (isset($options[$name]) && is_array($options[$name])){
+            }elseif (isset($options[$name]) && is_array($options[$name]) && is_array($map[$name])){
                 $merged[$name] = self::mapMerge($map[$name], $options[$name]);
+            }elseif (isset($options[$name]) && $map[$name] == null){
+                $merged[$name] = $options[$name];
             }else{
                 $merged[$name] = $map[$name];
             }
